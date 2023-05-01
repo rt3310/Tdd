43ea95fcd8ef1d938d73007fb4b04442f0cad3d2
rt3310 <btac3310@gmail.com>
2023-05-01 17:46:59 +0900

    update

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-public class Money {
+public class Money implements Expression {
 
     protected int amount;
     protected String currency;
@@ -9,11 +9,11 @@ public class Money {
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     @Override
@@ -33,5 +33,14 @@ public class Money {
     @Override
     public String toString() {
         return amount + " " + currency;
+    }
+
+    public Expression plus(Money addend) {
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Money reduce(String to) {
+        return this;
     }
 }
